96a5387a35336d831606e204cdfe3e73430b029a
artem <artem.titov.wg@gmail.com>
2022-08-07 13:28:32 +0300

    Составлен метод, который выводит массив в терминал

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,7 +3,12 @@
 
 int WordCount(string[] array, int wordLenght)
 
-void ShowArray(string[] array)
+void ShowArray(string[] array){ 
+    for (int i = 0; i < array.GetLength(0); i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
 
 string[] FillingArray (string[] array, int wordCount, int wordLenght)
 
