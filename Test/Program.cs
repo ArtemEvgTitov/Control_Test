e28c1aa00e93ed0d71d3f6e95ea1ab5c73ad78ec
artem <artem.titov.wg@gmail.com>
2022-08-07 13:27:18 +0300

    Составлен метод, определяющий объём нового массива

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,16 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
-int WordCount(string[] array, int wordLenght)
+int WordCount(string[] array, int wordLenght){
+ 
+    int wordCount = 0;
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= wordLenght)
+            wordCount++;
+
+    return wordCount;
+}
 
 void ShowArray(string[] array)
 
@@ -11,6 +20,6 @@ string[] FillingArray (string[] array, int wordCount, int wordLenght)
 string[] Array = { "Hi", "Pavel", "I", "am", "writing", "a", "test", 
                 "in", "C#", "It's", "good", "that", "I", "learned", "the", 
                 "material", ":)", "Thank", "you", ":)" };
-                
+
 Console.Write("\nВведите длинну строк для сортировки: ");
 int wordLenght = Convert.ToInt32(Console.ReadLine());
