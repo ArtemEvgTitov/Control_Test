8a63f68ea98d275de07e19a8c101532163667af3
artem <artem.titov.wg@gmail.com>
2022-08-07 13:21:12 +0300

    Добавлены типы и названия методов для решения

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,12 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив строк, длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
+int WordCount(string[] array, int wordLenght)
+
+void ShowArray(string[] array)
+
+string[] FillingArray (string[] array, int wordCount, int wordLenght)
+
 
 string[] Array = { "Hi", "Pavel", "I", "am", "writing", "a", "test", 
                 "in", "C#", "It's", "good", "that", "I", "learned", "the", 
